87f2e88016fe0e865ddde0cff9b85f06bd55aa94
Kimyerim <yeye092302@gmail.com>
2025-11-01 17:39:29 +0900

    feat: 구매 개수만큼 로또 발급 및 번호 출력 기능 구현
    LottoController에서 구매한 개수(lottoCount)만큼 1~45 사이의 6개 랜덤 숫자를 생성.
    생성된 번호로 Lotto 인스턴스를 생성하여 배열에 저장하고 ConsoleView를 통해 발급된 모든 로또의 번호를 콘솔에 출력.

--- src/controller/LottoController.js
+++ src/controller/LottoController.js
@@ -1,7 +1,9 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
 import { COMMON_MESSAGE } from "../constants/message.js";
 import Input from "../utils/Input.js";
 import Validator from "../utils/validate.js";
 import ConsoleView from "../view/ConsoleView.js";
+import Lotto from "../model/Lotto.js";
 
 class LottoController {
     async run() {
@@ -11,6 +13,18 @@ class LottoController {
         const lottoCount = inputPurchaseAmount.getLottoCount();
 
         ConsoleView.printPurchaseCount(lottoCount);
+
+        const lottos = [];
+        for(let i = 0; i < lottoCount; i++) {
+            const numbers = this.generateLottoNumbers();
+            const lotto = new Lotto(numbers);
+            lottos.push(lotto);
+        }
+        ConsoleView.printLottoNumbers(lottos);
+    }
+
+    generateLottoNumbers() {
+        return MissionUtils.Random.pickUniqueNumbersInRange(1, 45, 6).sort((a, b) => a - b);
     }
 }
 
--- src/view/ConsoleView.js
+++ src/view/ConsoleView.js
@@ -4,6 +4,12 @@ class ConsoleView {
     static printPurchaseCount(lottoCount) {
         Console.print(`\n${lottoCount}개를 구매했습니다.`);
     }
+
+    static printLottoNumbers(lottos) {
+        lottos.forEach((lotto) => {
+            Console.print(lotto.getNumbers());
+        });
+    }
 }
 
 export default ConsoleView;
