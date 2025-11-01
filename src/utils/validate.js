f1a321a6880320c670345f0cbcf5eb0c5d7bcf58
Kimyerim <yeye092302@gmail.com>
2025-11-01 17:53:30 +0900

    feat(validate): 당첨 번호 유효성 검사 기능 추가
    - 입력값을 쉼표를 기준으로 구분
    - checkWinningNumIsNumber: 입력값이 숫자인지 확인
    - checkWinnigNumRange: 각 번호가 1~45 범위인지 확인
    - checkIsSixElements: 6개의 숫자인지 확인
    - checkWinnigNumDuplication: 중복된 번호가 있는지 확인

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -2,6 +2,7 @@ import { LOTTO_CONFIG } from "../constants/config.js";
 import { ERROR_MESSAGE } from "../constants/message.js";
 
 class Validator {
+    // 구입 금액 검증
     static validatePurchaseAmount(purchaseAmount) {
         const amount = Number(purchaseAmount);
         this.checkDivisibleByThousand(amount);
@@ -24,6 +25,41 @@ class Validator {
     static checkIsPositive(value) {
         if (value <= 0) {
             throw new ERROR(ERROR_MESSAGE.NOT_POSITIVE);
+        }
+    }
+
+    // 당첨 번호 인증
+    static validateWinningNum(winningNum) {
+        const numbers = winningNum.split(',').map(Number);
+        this.checkWinningNumIsNumber(numbers);
+        this.checkWinningNumRange(numbers);
+        this.checkIsSixElements(numbers);
+        this.checkWinningNumDuplication(numbers);
+    }
+
+    static checkWinningNumIsNumber(value) {
+        if(value.some(isNaN)) {
+            throw new Error(ERROR_MESSAGE.NOT_NUMBER);
+        }
+    }
+
+    static checkWinningNumRange(value) {
+        const isValid = value.every(num => num >= 1 && num <= 45);
+        if (!isValid) {
+            throw new Error(ERROR_MESSAGE.NOT_RANGE);
+        }
+    }
+
+    static checkIsSixElements(value) {
+        if(value.length !== 6) {
+            throw new Error(ERROR_MESSAGE.NOT_SIX_ELEMENTS);
+        }
+    }
+
+    static checkWinningNumDuplication(value) {
+        const isValid = new Set(value).size === value.length;
+        if(!isValid) {
+            throw new Error(ERROR_MESSAGE.DUPLICATION);
         }
     }
 }
